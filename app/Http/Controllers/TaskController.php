f06368c5ad084e89e826dc5636bd2a5d39d220d0
peter-azer <peterazermosa1988@gmail.com>
2025-01-19 14:59:10 +0200

    Update TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -121,7 +121,7 @@ class TaskController extends Controller
 
 public function getApprovedTasks() 
     {
-        $tasks = Contract::where('status', 'approved')->with('contractService')->get();
+        $tasks = Contract::where('status', 'approved')->with('services')->get();
         return response()->json([
             'success' => true,
             'data' => $tasks,
